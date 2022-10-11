112cee5e1c5895a13db55c60518506baff5ccfbe
okirigabriel <okirigabriel24@gmail.com>
2022-10-11 04:42:28 -0500

    ec2 with terraform

---
+++ main.tf
@@ -0,0 +1,48 @@
+
+resource "aws_security_group" "aws_sg" {
+  name = "security group from terraform"
+
+  ingress {
+    description = "SSH from the internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "80 from the internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+resource "aws_instance" "aws_ins_web" {
+
+  ami                         = var.ami
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.aws_sg.id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name # your key here
+
+  tags = {
+    Name = "my instance"
+  }
+
+}
+
+output "instance_ip" {
+  value = aws_instance.aws_ins_web.public_ip
+}
+
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "ami" {
+  default = "ami-026b57f3c383c2eec"
+}
+
+variable "key_name" {
+  default = "bastion-kp"
+}
